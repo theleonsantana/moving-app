d53d39feb79dab2b1e40ce9c12161e919d006480
Leonardo Santana <hi@leonsantana.com>
2020-02-07 21:51:08 -0500

    added some seeded data to work locally

--- moving_app_api/db/seeds.rb
+++ moving_app_api/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+    Request.create(
+        contact: Faker::Name.unique.name,
+        address: Faker::Address.full_address,
+        start_at: Faker::Time.forward(days: 30,  period: :morning, format: :short),
+        truck: [true, false].sample,
+        helper: Faker::Number.within(range: 1..5),
+        contact_info: Faker::Internet.email,
+    )
+end
