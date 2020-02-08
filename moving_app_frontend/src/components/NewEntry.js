90b3ee55b06e9a8b9e5672ecb6539535899864c7
Leonardo Santana <hi@leonsantana.com>
2020-02-08 03:40:06 -0500

    finished adding the elements in the form

--- moving_app_frontend/src/components/NewEntry.js
+++ moving_app_frontend/src/components/NewEntry.js
@@ -5,16 +5,20 @@ export default class NewEntry extends Component {
 	state = {
 		contact: '',
 		contact_info: '',
-		start_at: null,
+		start_at: undefined,
 		address: '',
-		truck: Boolean,
-		helper: Number,
+		truck: false,
+		helper: 0,
 	};
+
 	handleChange = event => {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
 		this.setState({
-			[event.target.id]: event.target.value,
+			[event.target.id]: value,
 		});
 	};
+
 	handleSubmit = event => {
 		event.preventDefault();
 		const {
@@ -46,15 +50,64 @@ export default class NewEntry extends Component {
 		return (
 			<div>
 				<form onSubmit={this.handleSubmit}>
-					<label htmlFor="start_at">Choose a time for your appointment:</label>
-					<input
-						onChange={this.handleChange}
-						type="datetime-local"
-						id="start_at"
-						value={this.state.start_at}
-						min="2020-01-01T12:00"
-						max="2050-01-01T12:00"
-					/>
+					<label htmlFor="contact">
+						Contact Name
+						<input
+							type="text"
+							onChange={this.handleChange}
+							id="contact"
+							value={this.state.contact}
+						/>
+					</label>
+					<label htmlFor="contact_info">
+						Contact Information (Email):
+						<input
+							type="text"
+							onChange={this.handleChange}
+							id="contact"
+							value={this.state.contact_info}
+						/>
+					</label>
+					<label htmlFor="start_at">
+						Choose a time for your appointment:
+						<input
+							onChange={this.handleChange}
+							type="datetime-local"
+							id="start_at"
+							value={this.state.start_at}
+							min="2020-01-01T12:00"
+							max="2050-01-01T12:00"
+						/>
+					</label>
+					<label htmlFor="address">
+						Address:
+						<input
+							type="text"
+							onChange={this.handleChange}
+							id="address"
+							value={this.state.address}
+						/>
+					</label>
+					<label htmlFor="truck">
+						Do You need a truck:
+						<input
+							type="checkbox"
+							id="truck"
+							checked={this.state.isGoing}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<label htmlFor="helper">
+						Helpers:
+						<input
+							type="number"
+							max="5"
+							onChange={this.handleChange}
+							id="helper"
+							value={this.state.helper}
+						/>
+					</label>
+					<input type="submit" value="Submit Job Request" />
 				</form>
 			</div>
 		);
