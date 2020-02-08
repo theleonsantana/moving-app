9a380a9d9f5ff59e77800b69222d4ecbbec2b0cd
Leonardo Santana <hi@leonsantana.com>
2020-02-08 00:47:22 -0500

    added react router and started separating the app in components

---
+++ moving_app_frontend/src/components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Container, Grid, Typography, createMuiTheme } from '@material-ui/core';
+import './App.css';
+
+function App() {
+	return (
+		<Container>
+			<header className="App-header">
+				<Typography variant="h1" align="center" gutterBottom>
+					Moving App
+				</Typography>
+			</header>
+			<Grid container justify="center">
+				<Grid item xs={12} sm={8}>
+					<Typography varian="body2" paragraph gutterBottom>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+						imperdiet vehicula magna ullamcorper ornare. Morbi non blandit dui.
+						Suspendisse semper congue justo non iaculis. Cras et ex finibus,
+						mollis enim nec, tempor nunc. Pellentesque ligula sem, lobortis at
+						tempus in, malesuada non odio. Ut volutpat maximus nisl, nec dictum
+						dolor facilisis sit amet. Cras sapien felis, aliquam sit amet
+						blandit lacinia, vestibulum quis nibh. Vivamus sed odio ultrices,
+						condimentum odio eu, tristique est. Nullam odio nunc, malesuada vel
+						lobortis a, accumsan non metus. Duis velit libero, semper a aliquet
+						vitae, tristique sed lectus. Pellentesque congue sapien vitae erat
+						vulputate ultricies. Sed egestas libero ut risus tempor, finibus
+						blandit nunc lacinia.
+					</Typography>
+					<button>Get Started</button>
+				</Grid>
+			</Grid>
+		</Container>
+	);
+}
+
+export default App;
