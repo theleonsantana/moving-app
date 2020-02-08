9073bab2b95dd716713085a8b8ae91b9da8c56ed
Leonardo Santana <hi@leonsantana.com>
2020-02-07 23:34:29 -0500

    added material UI as the front end style lib, and started querying data in the front end

--- moving_app_frontend/src/App.js
+++ moving_app_frontend/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Requests from './components/Requests';
+import { Container } from '@material-ui/core';
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
+		<Container>
 			<header className="App-header">
 				<h1>Moving App</h1>
 			</header>
 			<main className="App-Container">
 				<Requests />
 			</main>
-		</div>
+		</Container>
 	);
 }
 
--- moving_app_frontend/src/components/Requests.js
+++ moving_app_frontend/src/components/Requests.js
@@ -11,13 +11,26 @@ export default class Requests extends Component {
 	getRequests = () => {
 		axios
 			.get('http://localhost:3000/requests')
-			.then(({ data }) => console.log(data))
+			.then(({ data }) => this.setState(this.setState({ requests: data })))
 			.catch(({ response }) => console.log(response));
 	};
 	render() {
 		return (
 			<div>
 				<h2>Client's Requests</h2>
+				{this.state.requests.map((request, index) => {
+					return (
+						<>
+							<p>{request.contact}</p>
+							<p>{request.contact_info}</p>
+							<p>
+								<p>{request.start_at}</p>
+							</p>
+							<p>{request.address}</p>
+							<p>Truck Needed: {request.truck ? 'Yes' : 'No'}</p>
+						</>
+					);
+				})}
 			</div>
 		);
 	}
