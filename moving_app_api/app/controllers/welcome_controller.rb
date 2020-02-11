a6b36537cb3d6682fc9f6dcd1f3ff6320600e5c9
Leonardo Santana <hi@leonsantana.com>
2020-02-10 23:51:14 -0500

    moved the api to its own repo and moved the client to its own repo

--- moving_app_api/app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    render json: { status: 200, message: "Moving App API" }
-  end
-end
--- moving_app_api/db/migrate/20200207221851_add_contact_info_to_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddContactInfoToRequests < ActiveRecord::Migration[6.0]
-  def change
-    add_column :requests, :contact_info, :string
-  end
-end
